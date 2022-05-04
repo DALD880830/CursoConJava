8cb6d54b88bf7093ea5ed6b916bde104ba4e5f33
DALD880830 <72953394+DALD880830@users.noreply.github.com>
2022-05-04 14:58:20 -0500

    ejemplos de diferentes arreglos

---
+++ src/EjemploArreglosCombinados.java
@@ -0,0 +1,34 @@
+public class EjemploArreglosCombinados {
+
+    public static void main(String[] args) {
+
+        int[] a, b, c;
+        a = new int[12]; //Arreglo a contiene 12 elementos
+        b = new int[12]; //Arreglo b contiene 12 elementos
+        c = new int[24]; //Arreglo c contendra los 12 elementos de a y los 12 elementos de b
+
+        for (int i = 0; i < a.length; i++) {  //i empieza en 0; mientras i sea menor al largo del arreglo a; i incrementa en 1
+            a[i] = i + 1; //el valor de i toma el valor de la posicion + 1
+        }
+
+        for (int i = 0; i < b.length; i++) {  //i empieza en 0; mientras i sea menor al largo del arreglo; i incrementa en 1
+            b[i] = (i + 1) * 5; //el valor de i toma el valor de la posicion + 1 y despues el resultado lo multiplica por 5
+        }
+
+        int aux = 0;
+        for (int i = 0; i < b.length; i+=3) { //i empieza en 0; mientras i sea menor al largo del arreglo b; i incrementa en 3
+            //i itera, realiza las 3 iteraciones del primer for de j, despues hace las 3 iteraciones del segundo for de j
+            //aumenta i en 3, porque se asignan 3 valores de a y 3 valores de b
+            for(int j = 0; j < 3; j++) { //j empieza en 0; mientras j sea menor que 3; j incrementa en 1
+                c[aux++] = a[i+j]; //almacena en c el valor de a
+            }
+            for(int j = 0; j < 3; j++) { //j empieza en 0; mientras j sea menor que 3; j incrementa en 1
+                c[aux++] = b[i+j]; //almacena en c el valor de b
+            }
+        }
+
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(i + ": " + c[i]);
+        }
+    }
+}
---
+++ src/EjemploArreglosOrdenPrincipioFinal.java
@@ -0,0 +1,25 @@
+public class EjemploArreglosOrdenPrincipioFinal {
+    public static void main(String[] args) {
+
+        int[] numeros = new int[10]; //Arreglo de 10 numeros enteros
+        int[] a = new int[10]; //Arreglo de 10 numeros enteros
+
+        for (int i = 0; i < numeros.length; i++) { //i inicia en 0; mientras que i sea menor que el largo del arreglo; i incrementa en 1
+            numeros[i] = i + 1; //Llenamos el arreglo con el valor de la iteracion, pero le sumamos 1 para que el primer valor sea 1
+        }
+
+        int aux = 0; //Variable auxiliar
+        for (int i = 0; i < numeros.length - i; i++) { //i inicia en 0; mientras que i sea menor que el largo del arreglo menos i; i incrementa en 1
+            //Con numeros.length-i hace un recorrido en 0 y a la vez resta la posicion 9, por lo tanto, solo hará un recorrido hasta la mitad del arreglo
+            a[aux++] = numeros[i]; //LLena la primera posición con el primer valor, llena 0. Incrementa la posición con el aux++
+            a[aux++] = numeros[numeros.length - 1 - i]; //Llena la segunda posicion con el ultimo valor y decrementa i. Incrementa la posicion con el aux++
+            //System.out.print(numeros[i] + " ");
+            //System.out.println(numeros[numeros.length - 1 - i]);
+        }
+
+        for (int i = 0; i < a.length; i++) { //Recorremos el arreglo para imprimir los valores
+            System.out.println("i = " + i + " valor: " + a[i]);
+        }
+
+    }
+}
